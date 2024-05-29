5c3b30c7934bb42450b0889237e207132841fe3a
vaish06navi <132326467+vaish06navi@users.noreply.github.com>
2024-05-29 17:25:44 +0530

    Time: 48 ms (23.52%) | Memory: 20.9 MB (35.98%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.py
+++ 113-path-sum-ii/path-sum-ii.py
@@ -5,23 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
-        if not root:
-            return root
+	def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+		if not root:
+			return root
 
-        ans = []
-        def dfs(node,root):
-            if node.left:
-                dfs(node.left,res+[node.left])
-            if node.right:
-                dfs(node.right,res+[node.right])
-            else:
-                if sum(res) == targetSum or not root.left or root.right:
-                    ans.append(res)
-        dfs(root,[root.val])
-        return ans
+		ans = []
 
-        
+		def dfs(node, res):
+			if node.left:
+				dfs(node.left,  res+[node.left.val])
+			if node.right:
+				dfs(node.right,  res+[node.right.val])
+			else:
+				if sum(res) == targetSum and not node.left and not node.right:
+					ans.append(res)
+
+		dfs(root, [root.val])
+		return ans
 
 
 
@@ -33,20 +33,3 @@ class Solution:
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def find_path_recur(self, node, target, cur_path, all_paths):
-        if not node:
-            return
-        cur_path.append(node.val)
-        if node.val == target and not node.left and not node.right:
-            all_paths.append(list(cur_path))
-        else:
-            self.find_path_recur(node.left, target-node.val, cur_path, all_paths)
-            self.find_path_recur(node.right, target-node.val, cur_path, all_paths)
-        cur_path.pop(-1)
-
-
-    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
-        out = []
-        self.find_path_recur(root, targetSum, [], out)
-        return out
