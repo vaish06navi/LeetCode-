a319bf53873de182d5fef3ae76064fd951aa3d89
vaish06navi <132326467+vaish06navi@users.noreply.github.com>
2024-07-14 18:33:25 +0530

    Time: 36 ms (63.77%) | Memory: 16.5 MB (59.62%) - LeetSync

---
+++ 71-simplify-path/simplify-path.py
@@ -0,0 +1,16 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        path = path.split('/')
+        stack = []
+
+        for i in range(len(path)):
+            if path[i] == '' or path[i] == '.':
+                continue
+            elif path[i] == '..':
+                if stack:
+                    stack.pop(-1)
+            else:
+                stack.append(path[i])
+        path = '/'.join(stack)
+        return '/' + path
+        
