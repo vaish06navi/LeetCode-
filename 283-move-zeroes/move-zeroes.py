c70eb8a93869fc4195943e869a7c8a1e0bf6a327
Sree Vaishnavi Karipe <132326467+vaish06navi@users.noreply.github.com>
2024-07-22 22:49:06 +0530

    Time: 110 ms (98.90%) | Memory: 18 MB (50.04%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -11,13 +11,4 @@ class Solution:
             if nums[slow] != 0:
                 slow += 1
 
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        left = 0
 
-        for right in range(len(nums)):
-            if nums[right] != 0:
-                nums[right], nums[left] = nums[left], nums[right]
-                left += 1
-        
-        return nums
