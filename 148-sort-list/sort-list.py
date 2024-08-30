c9d0a3cf8a36aa7e6858f465cc4713cf4cf7a8c7
Sree Vaishnavi Karipe <132326467+vaish06navi@users.noreply.github.com>
2024-08-30 21:36:03 +0530

    Time: 282 ms (56.37%) | Memory: 32.5 MB (30.36%) - LeetSync

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -1,23 +1,45 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        a = []
-        while head:
-            a.append(head.val)
-            head = head.next
-        a.sort()
-
-        if not a:
-            return
+        if not head or not head.next:
+            return head
         
-        root = ListNode(a[0])
-        curr = root
-        for val in a[1:]:
-            curr.next = ListNode(val)
-            curr = curr.next
-        return root
+        # Split the list into two halfs
+        left = head
+        right = self.getMid(head)
+        tmp = right.next
+        right.next = None
+        right = tmp
+        
+        left = self.sortList(left)
+        right = self.sortList(right)
+        
+        return self.merge(left, right)
+    
+    def getMid(self, head):
+        slow = head
+        fast = head.next
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+    
+    # Merge the list
+    def merge(self, list1, list2):
+        newHead = tail = ListNode()
+        while list1 and list2:
+            if list1.val > list2.val:
+                tail.next = list2
+                list2 = list2.next
+            else:
+                tail.next = list1
+                list1 = list1.next
+            tail = tail.next
+        
+        if list1:
+            tail.next = list1
+        if list2:
+            tail.next = list2
+        
+        return newHead.next
         
