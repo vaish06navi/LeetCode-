3735dc252892e80cd930da827fb3a137b6987289
Sree Vaishnavi Karipe <132326467+vaish06navi@users.noreply.github.com>
2024-09-30 15:53:32 +0530

    Time: 530 ms (56.29%) | Memory: 24 MB (79.06%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -23,3 +23,41 @@ class Solution:
             if not dfs(i):
                 res.append(i)
         return res
+
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        V = len(graph)
+        adj = [[] for _ in range(V)]
+
+        # reverse the direction
+        for i in range(V):
+            for j in graph[i]:
+                adj[j].append(i)
+
+        # calculate the indegree
+        indegree = [0]*V
+
+        for i in range(V):
+            for j in adj[i]:
+                indegree[j] += 1
+            
+        # store the node with indegree = 0, in the queue
+        q = deque()
+
+        for i in range(V):
+            if indegree[i] == 0:
+                q.append(i)
+        
+        # using kahn's algorithms and store the element in the list
+        # basically we are detecting the cycle in the directed graph
+        ans = []
+        while (q):
+            ele = q.popleft()
+            ans.append(ele)
+
+            for i in adj[ele]:
+                if indegree[i] != 0:
+                    indegree[i] -= 1
+                    if indegree[i] == 0:
+                        q.append(i)
+        return sorted(ans)
