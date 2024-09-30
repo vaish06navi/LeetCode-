4cbc95e34d5dd535b0ed93d7bdc6a250b4b8ea24
Sree Vaishnavi Karipe <132326467+vaish06navi@users.noreply.github.com>
2024-09-30 15:49:35 +0530

    Time: 519 ms (74.77%) | Memory: 24.4 MB (38.63%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -1,3 +1,8 @@
+# Approach:
+# - Reverse the graph: Track incoming edges instead of outgoing ones.
+# - Use Topological Sorting with a queue to perform a reverse BFS starting from terminal nodes.
+# - Nodes that reach terminal nodes in this reversed graph are marked as safe.
+
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         res = []
@@ -6,19 +11,15 @@ class Solution:
         def dfs(node):
             if node in visited:
                 return visited[node]
-            
-            visited[node]=True
-
-            for nei in graph[node]:
-                if dfs(nei):
+ 
+            visited[node] = True
+            for neig in graph[node]:
+                if dfs(neig):
                     return True
-            
-            visited[node]=False
+                
+            visited[node] = False
 
         for i in range(len(graph)):
             if not dfs(i):
                 res.append(i)
-
         return res
-
-        
