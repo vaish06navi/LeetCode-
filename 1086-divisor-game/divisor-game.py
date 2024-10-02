63ebb38fa1126ee116d04f6fb08c0cef899070e8
Sree Vaishnavi Karipe <132326467+vaish06navi@users.noreply.github.com>
2024-10-02 10:30:01 +0530

    Time: 33 ms (71.84%) | Memory: 16.5 MB (78.66%) - LeetSync

--- 1086-divisor-game/divisor-game.py
+++ 1086-divisor-game/divisor-game.py
@@ -3,7 +3,19 @@ class Solution:
         # n = 2 --> alice = 1
         # n = 4 --> alice = 1,3
         # n = 5 --> alice = 1,3,5 ==> if alice choses an odd value she will lose
-        if n % 2 != 0:
+        if n % 2 != 0: # n&1
             return False
         return True
+
+        if n < 2:
+            return False
+
+        dp = [False]*(n+1)
+        dp[2] = True
+
+        for i in range(3,n+1):
+            for j in range(1,int(math.sqrt(i)) + 1):
+                if i % j == 0 and dp[i-j] == False:
+                    dp[i] = True
+        return dp[i-1]
         
