28fd8247b764c31dc2df1246684fde036f36ff8d
Sree Vaishnavi Karipe <132326467+vaish06navi@users.noreply.github.com>
2024-10-03 00:07:37 +0530

    Time: 521 ms (8.16%) | Memory: 40.6 MB (5.04%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,16 +1,26 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        res = []
-
-        def backtrack(i,curr):
-            if i==len(s):
-                res.append(curr[:])
+        n = len(s)
+        is_palindrome = [[False] * n for _ in range(n)]
+        
+        for i in range(n):
+            is_palindrome[i][i] = True
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                if s[i] == s[j]:
+                    if length == 2 or is_palindrome[i + 1][j - 1]:
+                        is_palindrome[i][j] = True
+        
+        def dfs(start):
+            if start == len(s):
+                return [[]]
             
-            for j in range(i,len(s)):
-                if s[i:j+1]==s[i:j+1][::-1]:
-                    curr.append(s[i:j+1])
-                    backtrack(j+1,curr)
-                    curr.pop()
-
-        backtrack(0,[])
-        return res
+            result = []
+            for end in range(start, len(s)):
+                if is_palindrome[start][end]:
+                    for sub in dfs(end + 1):
+                        result.append([s[start:end + 1]] + sub)
+            return result
+        
+        return dfs(0)
