66528a6daca97db5402e6020c8bfa344df119f36
Sree Vaishnavi Karipe <132326467+vaish06navi@users.noreply.github.com>
2024-11-03 17:58:39 +0530

    Time: 0 ms (100.00%) | Memory: 16.9 MB (49.20%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -4,19 +4,22 @@ class Solution:
         right = len(nums)-1
 
         while left <= right:
-            mid = (left+right)//2
+            mid = (left + right) // 2
 
             if nums[mid] == target:
                 return mid
 
+            # check if left half is sorted
             if nums[left] <= nums[mid]:
-                if nums[left] <= target <= nums[mid]:
-                    right = mid-1
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
                 else:
-                    left = mid+1
+                    left = mid + 1
+                
+            # check if right half is sorted
             else:
                 if nums[mid] <= target <= nums[right]:
-                    left = mid+1
+                    left = mid + 1
                 else:
-                    right=mid-1
-        return -1       
+                    right = mid - 1
+        return -1
