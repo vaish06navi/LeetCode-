8e2e1c499e0167d97fa8574e52a2ba33d3a6b5af
Sree Vaishnavi Karipe <132326467+vaish06navi@users.noreply.github.com>
2024-11-05 20:22:51 +0530

    Time: 19 ms (77.03%) | Memory: 16.8 MB (89.02%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
@@ -1,12 +1,21 @@
+# class Solution:
+#     def minChanges(self, s: str) -> int:
+#         l = 0
+#         res = 0
+
+#         for r in range(len(s)):
+#             if s[l] != s[r]:
+#                 if r %2 == 1:
+#                     res += 1
+#                 l = r
+#         return res
+        
 class Solution:
     def minChanges(self, s: str) -> int:
-        l = 0
         res = 0
 
-        for r in range(len(s)):
-            if s[l] != s[r]:
-                if r %2 == 1:
-                    res += 1
-                l = r
+        for i in range(0, len(s) , 2):
+            if s[i] != s[i+1]:
+                res += 1
         return res
         
