ac75b8c371eb89ebbcadf163b72a3927456995d8
Sree Vaishnavi Karipe <132326467+vaish06navi@users.noreply.github.com>
2026-05-13 21:56:08 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (19.29%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,11 +1,36 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mp = {}
+        n = len(nums)
+        for i in range(n-1):
+            for j in range(i+1 , n):
+                if nums[i] + nums[j] == target:
+                    return [i, j ]
+        return []
+
+    
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h = {}
+        n = len(nums)
+
+        # build the hash map
+        for i in range(n):
+            h[nums[i]] = i
+        
+        # find the complement
+        for i in range(n):
+            comp = target - nums[i]
+            if comp in h and h[comp] != i:
+                return [i, h[comp]]
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h = {}
         n = len(nums)
 
         for i in range(n):
             comp = target - nums[i]
-            if comp in mp:
-                return [mp[comp] , i]
-            mp[nums[i]] = i
+            if comp in h:
+                return [h[comp] , i]
+            h[nums[i]] = i
         return []
